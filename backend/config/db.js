be2f531c6443a07c2f5334ef4e45971fed019adb
Lachezar <uzunov_89_lacho@abv.bg>
2023-04-24 10:34:01 +0300

    setting up nodejs server and connecting to db

---
+++ backend/config/db.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('photofari', 'root', 'Uzu_l0v_Roni', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+// async function connectDB () {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Sucess');
+//     } catch (err){
+//         console.log('Failed to connect')
+//     }
+// }
+
+// ------ Connecting with ARROW function ---------- //
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Success')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = connectDB;
+
+// ------ Connecting with THEN block ---------- //
+// sequelize.authenticate().then(() => {
+//     console.log('Connection successfull');
+// }).catch((err) => {
+//     console.log('Error connecting to the database')
+// });
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+const connectDB = require('./config/db');
+
+// Connecting to the mySQL databsae
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.use((req, res, next) => {
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.header("Access-Control-Allow-Origin", "*");
+res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
+res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+next();
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
